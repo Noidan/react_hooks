4a740b9f904169e0373a277eb4431c74b0308126
Tony <122675474+Noidan@users.noreply.github.com>
2023-10-04 22:57:48 +0300

    Request

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import './App.css';
-import Hover from './components/Hover';
+import React, { useState } from 'react';
+import List from "./components/List";
+import useDebounce from "./hooks/useDebounce";
+import axios from "axios";
+import useRequest from "./hooks/useRequest";
 import useInput from './hooks/useInput';
+import Hover from './components/Hover';
 
 function App() {
 
   const username = useInput('')
   const password = useInput('')
+  const [todos, loading, error] = useRequest(fetchTodos)
+
+  function fetchTodos() {
+    return axios.get(`https://jsonplaceholder.typicode.com/todos`)
+  }
+
+  if (loading) {
+    return <h1>Идет загрузка...</h1>
+  }
+
+  if (error) {
+    return <h1>Произошла ошибка при загрузке данных</h1>
+  }
+
+
 
   return (
     <div className="App">
@@ -16,6 +35,13 @@ function App() {
 
       <div>
         <Hover />
+      </div>
+      <div>
+        {todos && todos.map(todo =>
+          <div key={todo.id} style={{ padding: 30, border: '2px solid black' }}>
+            {todo.id}. {todo.title}
+          </div>
+        )}
       </div>
     </div>
   );
